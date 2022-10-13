3dcba8b956ae55d75b0041a46183a53ba9f1ed35
Raziye Yolasigmazoglu <57474816+raziyeyolasigmayoglu@users.noreply.github.com>
2022-10-13 14:43:11 +0100

    it integrated for IOS

---
+++ build/ios/Release-iphoneos/dialogflow_grpc/dialogflow_grpc.framework/Headers/dialogflow_grpc-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DialogflowGrpcPlugin.h"
+
+FOUNDATION_EXPORT double dialogflow_grpcVersionNumber;
+FOUNDATION_EXPORT const unsigned char dialogflow_grpcVersionString[];
+
